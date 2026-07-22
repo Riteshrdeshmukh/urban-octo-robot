57f7e64ebb3dab47400ef57ed372da8276e264a5
Riteshrdeshmukh <riteshrdeshmukh46@gmail.com>
2026-07-22 19:50:10 +0530

    index.php complete

--- index.php
+++ index.php
@@ -219,3 +219,21 @@ foreach($results as $result)
 <!--Register-Form -->
 <?php include('includes/registration.php');?>
 
+<!--/Forgot-password-Form --> 
+
+<!-- Scripts --> 
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script> 
+<script src="assets/js/interface.js"></script> 
+<!--Switcher-->
+<script src="assets/switcher/js/switcher.js"></script>
+<!--bootstrap-slider-JS--> 
+<script src="assets/js/bootstrap-slider.min.js"></script> 
+<!--Slider-JS--> 
+<script src="assets/js/slick.min.js"></script> 
+<script src="assets/js/owl.carousel.min.js"></script>
+
+</body>
+
+<!-- Mirrored from themes.webmasterdriver.net/carforyou/demo/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Jun 2017 07:22:11 GMT -->
+</html>
