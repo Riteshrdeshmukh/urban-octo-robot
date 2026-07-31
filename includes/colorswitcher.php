c07a983346fd58d151a23afe3e513e56726ac15e
Riteshrdeshmukh <riteshrdeshmukh46@gmail.com>
2026-07-31 20:24:28 +0530

    includes

---
+++ includes/colorswitcher.php
@@ -0,0 +1,19 @@
+<div class="switcher-wrapper">
+  <div class="demo_changer">
+    <div class="demo-icon customBgColor">
+      <i class="fa fa-cog fa-spin fa-2x"></i>
+    </div>
+
+    <div class="form_holder">
+      <div class="skin-theme-switcher">
+        <h4>Color</h4>
+        <a href="#" data-switchcolor="red" class="styleswitch" style="background:#de302f"></a>
+        <a href="#" data-switchcolor="orange" class="styleswitch" style="background:#f76d2b"></a>
+        <a href="#" data-switchcolor="blue" class="styleswitch" style="background:#228dcb"></a>
+        <a href="#" data-switchcolor="pink" class="styleswitch" style="background:#ff2761"></a>
+        <a href="#" data-switchcolor="green" class="styleswitch" style="background:#2dcc70"></a>
+        <a href="#" data-switchcolor="purple" class="styleswitch" style="background:#6054c2"></a>
+      </div>
+    </div>
+  </div>
+</div>
